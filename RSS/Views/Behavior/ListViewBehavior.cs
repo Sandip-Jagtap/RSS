7ef4062830f58336014847b5ab4f36ba8606b950
Sandip <s.jagtap048@gmail.com>
2020-06-08 11:39:05 +0530

    the project working

---
+++ RSS/Views/Behavior/ListViewBehavior.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using RSS.Models;
+using Xamarin.Forms;
+
+namespace RSS.Views.Behavior
+{
+    public class ListViewBehavior : Behavior<ListView>
+    {
+        ListView listView;
+
+        protected override void OnAttachedTo(ListView bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            listView = bindable;
+            listView.ItemSelected += ListView_ItemSelected;
+        }
+
+        void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Item selectedItem = (listView.SelectedItem) as Item;
+            Application.Current.MainPage.Navigation.PushAsync(new PostPage(selectedItem));
+        }
+
+        protected override void OnDetachingFrom(ListView bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            listView.ItemSelected -= ListView_ItemSelected;
+        }
+    }
+}
---
+++ RSS/Views/PostPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using RSS.Models;
+using Xamarin.Forms;
+
+namespace RSS.Views
+{
+    public partial class PostPage : ContentPage
+    {
+        public PostPage()
+        {
+            InitializeComponent();
+            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+        }
+
+        public PostPage(Item item)
+        {
+            InitializeComponent();
+            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+
+            webView.Source = item.ItemLink;
+        }
+    }
+}
